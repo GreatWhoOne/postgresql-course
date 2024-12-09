9556d700328f837a0bb8b2be341a12c5d11037d5
Juan Cruz Pereira <juancp98@hotmail.com>
2024-12-09 11:58:40 -0300

    update and delete + exercises

---
+++ update_and_delete.sql
@@ -0,0 +1,10 @@
+SELECT * FROM cliente ORDER BY idcliente ASC;
+
+
+UPDATE cliente SET nome = 'Teste' WHERE idcliente = 1;
+
+UPDATE cliente SET nome = 'Adriano', genero = 'M', numero = '241' WHERE idcliente = 4;
+
+INSERT INTO cliente (idcliente, nome) VALUES (16, 'João');
+
+DELETE FROM cliente WHERE idcliente = 16;
